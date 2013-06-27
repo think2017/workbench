617d8547afd74d29cef33a4a0ec2dc374cea426b
think2017 <think2017@gmail.com>
2013-06-27 17:07:18 +0800

    Update common.php

--- common.php
+++ common.php
@@ -5,14 +5,16 @@
  * @char-set UTF-8
  * 
  **/
-function cutstr($string, $length) {
-        preg_match_all("/[\x01-\x7f]|[\xc2-\xdf][\x80-\xbf]|\xe0[\xa0-\xbf][\x80-\xbf]|[\xe1-\xef][\x80-\xbf][\x80-\xbf]|\xf0[\x90-\xbf][\x80-\xbf][\x80-\xbf]|[\xf1-\xf7][\x80-\xbf][\x80-\xbf][\x80-\xbf]/", $string, $info);  
-        for($i=0; $i<count($info[0]); $i++) {
-                $wordscut .= $info[0][$i];
-                $j = ord($info[0][$i]) > 127 ? $j + 2 : $j + 1;
-                if ($j > $length - 3) {
-                        return $wordscut." ...";
-                }
+function cutstr($string, $length) 
+{
+    preg_match_all("/[\x01-\x7f]|[\xc2-\xdf][\x80-\xbf]|\xe0[\xa0-\xbf][\x80-\xbf]|[\xe1-\xef][\x80-\xbf][\x80-\xbf]|\xf0[\x90-\xbf][\x80-\xbf][\x80-\xbf]|[\xf1-\xf7][\x80-\xbf][\x80-\xbf][\x80-\xbf]/", $string, $info);  
+    for($i=0; $i<count($info[0]); $i++) {
+        $wordscut .= $info[0][$i];
+        $j = ord($info[0][$i]) > 127 ? $j + 2 : $j + 1;
+        if ($j > $length - 3) {
+            return $wordscut." ...";
         }
-        return join('', $info[0]);
+    }
+  
+    return join('', $info[0]);
 }
